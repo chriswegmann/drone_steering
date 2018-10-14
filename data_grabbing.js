04b182ba517e97844fc21be5e76f5319d48810e1
Christian Wegmann <christian.wegmann@hispeed.ch>
2018-10-14 09:24:50 +0200

    improved data preparation

--- data_grabbing.js
+++ data_grabbing.js
@@ -1,12 +1,12 @@
 	const multiplier = 0.5;
-  const imageScaleFactor = 0.5;
-  const flipHorizontal = false;
-  const outputStride = 8;
+  	const imageScaleFactor = 0.5;
+  	const flipHorizontal = false;
+  	const outputStride = 8;
 	const videoWidth = 800;
 	const videoHeight = 555;
 
 	let estimator = null;
-  localStorage.position = ''
+  	localStorage.position = ''
 
 	var label = [];
 	var sample = [];
@@ -23,44 +23,56 @@
 		startCamera();	
 		video = document.getElementById('video');
 
-	  tid = setInterval(function() {
-	      getCoordSnapshot(video, 0, 'webcam', modelType);
-	  }, interval);
+	  	tid = setInterval(function() {
+	    	getCoordSnapshot(video, 'webcam', modelType);
+	  	}, interval);
+
+	}
+
+
+	function getCoordinatesAllVideos() {
+	// TODO: this should be made work with asynchronous functions
+		var videos = [];
+		videos[0] = "001";
+		videos[1] = "002";
+
+		for (var i in videos) {
+			getCoordinatesVideo(videos[i]);
+		}		
 
 	}
 
 
 	function getCoordinatesVideo() {
-	// runs the video and gets the coordinates for each snapshot of a time interval as long as the video is running
+	// runs the video and gets the coordinates for each snapshot at a selected time interval as long as the video is running
 
 		var i = 0;
-		var interval = 300
-	  var video = document.getElementById('video');
 
-		var selected_video = document.getElementById("selected_video");
-		var video_name = selected_video.options[selected_video.selectedIndex].value;
+		var stepTime = document.getElementById("step_time");
+		var interval = stepTime.options[stepTime.selectedIndex].value;
+	  	var video = document.getElementById('video');
+		var selectedVideo = document.getElementById("selected_video");
+		var videoId = String(selectedVideo.options[selectedVideo.selectedIndex].value);
+		var videoName = "video_" + String(videoId);
 
-	  loadLabels(video_name);
+		loadLabels(videoId);
 
-	  video.src = 'videos/' + video_name + '.mp4';
+	  	video.src = 'videos/' + videoName + '.mp4';
 		video.play();
 
-	  tid = setInterval(function() {
-	    if (!video.ended && !video.paused) {
-	      document.getElementById('count').innerHTML = i;
-	      getCoordSnapshot(video, i, 'video');
-				showLabel(i);
-	    	++i;
-	    }
-	  }, interval);
+	  	tid = setInterval(function() {
+	    	if (!video.ended && !video.paused) {
+	      		document.getElementById('count').innerHTML = i;
+	      		getCoordSnapshot(video, 'video');
+				showLabel(video.currentTime);
+	    		++i;
+	    	}
+	  	}, interval);
 	  
-	  //JSON.stringify(all_samples);
-	  //console.log(all_samples);
-
 	}
   
   
-  function getCoordSnapshot(imgData, labelIndex, sourceType, modelType='none') {
+  function getCoordSnapshot(imgData, sourceType, modelType='none') {
 	// gets to coordinates of imgData at the given time and store or display the information
 	
     posenet
@@ -72,70 +84,35 @@
       .then((resp) => {
       	
       	if (sourceType=='video') {
+			
+			j = 0;
 
-	      	document.getElementById("nose_x").innerHTML = resp.keypoints[0].position.x;
-	      	document.getElementById("nose_y").innerHTML = resp.keypoints[0].position.y;
-	      	document.getElementById("leftEye_x").innerHTML = resp.keypoints[1].position.x;
-	      	document.getElementById("leftEye_y").innerHTML = resp.keypoints[1].position.y;
-	      	document.getElementById("rightEye_x").innerHTML = resp.keypoints[2].position.x;
-	      	document.getElementById("rightEye_y").innerHTML = resp.keypoints[2].position.y;
-	      	document.getElementById("leftEar_x").innerHTML = resp.keypoints[3].position.x;
-	      	document.getElementById("leftEar_y").innerHTML = resp.keypoints[3].position.y;
-	      	document.getElementById("rightEar_x").innerHTML = resp.keypoints[4].position.x;
-	      	document.getElementById("rightEar_y").innerHTML = resp.keypoints[4].position.y;
-	      	document.getElementById("leftShoulder_x").innerHTML = resp.keypoints[5].position.x;
-	      	document.getElementById("leftShoulder_y").innerHTML = resp.keypoints[5].position.y;
-	      	document.getElementById("rightShoulder_x").innerHTML = resp.keypoints[6].position.x;
-	      	document.getElementById("rightShoulder_y").innerHTML = resp.keypoints[6].position.y;
-	      	document.getElementById("leftElbow_x").innerHTML = resp.keypoints[7].position.x;
-	      	document.getElementById("leftElbow_y").innerHTML = resp.keypoints[7].position.y;
-	      	document.getElementById("rightElbow_x").innerHTML = resp.keypoints[8].position.x;
-	      	document.getElementById("rightElbow_y").innerHTML = resp.keypoints[8].position.y;
-	      	document.getElementById("leftWrist_x").innerHTML = resp.keypoints[9].position.x;
-	      	document.getElementById("leftWrist_y").innerHTML = resp.keypoints[9].position.y;
-	      	document.getElementById("rightWrist_x").innerHTML = resp.keypoints[10].position.x;
-	      	document.getElementById("rightWrist_y").innerHTML = resp.keypoints[10].position.y;
-	      	document.getElementById("leftHip_x").innerHTML = resp.keypoints[11].position.x;
-	      	document.getElementById("leftHip_y").innerHTML = resp.keypoints[11].position.y;
-	      	document.getElementById("rightHip_x").innerHTML = resp.keypoints[12].position.x;
-	      	document.getElementById("rightHip_y").innerHTML = resp.keypoints[12].position.y;
-	      	document.getElementById("leftKnee_x").innerHTML = resp.keypoints[13].position.x;
-	      	document.getElementById("leftKnee_y").innerHTML = resp.keypoints[13].position.y;
-	      	document.getElementById("rightKnee_x").innerHTML = resp.keypoints[14].position.x;
-	      	document.getElementById("rightKnee_y").innerHTML = resp.keypoints[14].position.y;
-	      	document.getElementById("leftAnkle_x").innerHTML = resp.keypoints[15].position.x;
-	      	document.getElementById("leftAnkle_y").innerHTML = resp.keypoints[15].position.y;
-	      	document.getElementById("rightAnkle_x").innerHTML = resp.keypoints[16].position.x;
-	      	document.getElementById("rightAnkle_y").innerHTML = resp.keypoints[16].position.y;
+			for (i=0; i < 17; i++) {
+				if (document.getElementById("keypoint_" + i).checked) {
+					sample[j] = Math.round(resp.keypoints[i].position.x, 0) / 800;
+					document.getElementById("keypoint_" + i + "_x").innerHTML = sample[j];
+					j += 1;
+					sample[j] = Math.round(resp.keypoints[i].position.y, 0) / 800;
+					document.getElementById("keypoint_" + i + "_y").innerHTML = sample[j];
+					j += 1;
+				}
+			}
 
-					sample[0] = Math.round(resp.keypoints[5].position.x, 0) / 800;
-					sample[1] = Math.round(resp.keypoints[5].position.y, 0) / 800;
-					sample[2] = Math.round(resp.keypoints[6].position.x, 0) / 800;
-					sample[3] = Math.round(resp.keypoints[6].position.y, 0) / 800;
-					sample[4] = Math.round(resp.keypoints[7].position.x, 0) / 800;
-					sample[5] = Math.round(resp.keypoints[7].position.y, 0) / 800;
-					sample[6] = Math.round(resp.keypoints[8].position.x, 0) / 800;
-					sample[7] = Math.round(resp.keypoints[8].position.y, 0) / 800;
-					sample[8] = Math.round(resp.keypoints[9].position.x, 0) / 800;
-					sample[9] = Math.round(resp.keypoints[9].position.y, 0) / 800;
-					sample[10] = Math.round(resp.keypoints[10].position.x, 0) / 800;
-					sample[11] = Math.round(resp.keypoints[10].position.y, 0) / 800;
+			sample[j] = getLabel(imgData.currentTime);
+				
+			sample_json = JSON.stringify(sample);
+			all_samples_json = all_samples_json + sample_json;
 
-					sample[12] = label[labelIndex];
-						
-					sample_json = JSON.stringify(sample);
-					all_samples_json = all_samples_json + sample_json;
-	
-					// store the coordinates locally in a log file (available under C:\Users\Christian\AppData\Local\Google\Chrome\User Data\Default\Local Storage\leveldb)
-					//position = JSON.stringify(resp);
-					//console.log(position);
-					//localStorage.position = localStorage.position + position;
+			//store the coordinates locally in a log file (available under C:\Users\Christian\AppData\Local\Google\Chrome\User Data\Default\Local Storage\leveldb)
+			//position = JSON.stringify(resp);
+			//console.log(position);
+			//localStorage.position = localStorage.position + position;
 
 	      }
 
       	if (sourceType=='webcam') {
 						
-	      		var direction = 'stop.png'
+	      	var direction = 'stop.png'
       		
            	var leftShoulder_x = Math.round(resp.keypoints[5].position.x);
             var leftShoulder_y = Math.round(resp.keypoints[5].position.y);
@@ -150,107 +127,102 @@
             var rightWrist_x = Math.round(resp.keypoints[10].position.x);
             var rightWrist_y = Math.round(resp.keypoints[10].position.y);
 
-						if (modelType=='deltas') {
+			if (modelType=='deltas') {
 
-							var leftArm_x = leftWrist_x - leftShoulder_x;
-							var rightArm_x = rightShoulder_x - rightWrist_x;
-							var leftArm_y = leftShoulder_y - leftWrist_y;
-							var rightArm_y = rightShoulder_y - rightWrist_y;
-	
-							var left_ind = 'leftArm_x: ' + leftArm_x + '<br>leftWrist_x: ' + leftWrist_x + '<br>leftShoulder_x: ' + leftShoulder_x;
-							var right_ind = 'rightArm_x: ' + rightArm_x + '<br>rightWrist_x: ' + rightWrist_x + '<br>rightShoulder_x: ' + rightShoulder_x;
-							var stop_ind = left_ind + '<br>' + right_ind;
-							var up_ind = 'leftArm_y: ' + leftArm_y + '<br>leftWrist_y: ' + leftWrist_y + '<br>leftShoulder_y: ' + leftShoulder_y + '<br>rightArm_y: ' + rightArm_y + '<br>rightWrist_y: ' + rightWrist_y + '<br>rightShoulder_y: ' + rightShoulder_y;
-							var down_ind = up_ind;
+				var leftArm_x = leftWrist_x - leftShoulder_x;
+				var rightArm_x = rightShoulder_x - rightWrist_x;
+				var leftArm_y = leftShoulder_y - leftWrist_y;
+				var rightArm_y = rightShoulder_y - rightWrist_y;
 
-							var left_ind = '';
-							var right_ind = '';
-							var stop_ind = '';
-							var up_ind = '';
-							var down_ind = '';
+				var left_ind = 'leftArm_x: ' + leftArm_x + '<br>leftWrist_x: ' + leftWrist_x + '<br>leftShoulder_x: ' + leftShoulder_x;
+				var right_ind = 'rightArm_x: ' + rightArm_x + '<br>rightWrist_x: ' + rightWrist_x + '<br>rightShoulder_x: ' + rightShoulder_x;
+				var stop_ind = left_ind + '<br>' + right_ind;
+				var up_ind = 'leftArm_y: ' + leftArm_y + '<br>leftWrist_y: ' + leftWrist_y + '<br>leftShoulder_y: ' + leftShoulder_y + '<br>rightArm_y: ' + rightArm_y + '<br>rightWrist_y: ' + rightWrist_y + '<br>rightShoulder_y: ' + rightShoulder_y;
+				var down_ind = up_ind;
 
-	
-							if (leftArm_x > 60) {
-								//left_ind = 'detected<br>' + left_ind;
-								left_ind = 'detected';
-								direction = 'left.png'
-							}
-	
-							if (rightArm_x > 60) {
-								//right_ind = 'detected<br>' + right_ind;
-								right_ind = 'detected';
-								direction = 'right.png';
-							}
-	
-							if ((leftArm_x > 60) & (rightArm_x > 60)) {
-								//stop_ind = 'detected<br>' + stop_ind;
-								stop_ind = 'detected';
-								direction = 'stop.png';
-							}
-	
-	
-							if ((leftArm_y > 100) & (rightArm_y > 100))  {
-								//up_ind = 'detected<br>' + up_ind;
-								up_ind = 'detected';
-								direction = 'up.png';
-							}
-	
-							if ((leftArm_y < -100) & (rightArm_y < -100))  {
-								//down_ind = 'detected<br>' + down_ind;
-								down_ind = 'detected';
-								direction = 'down.png';
-							}
-	
-							document.getElementById("leftArm_x").innerHTML = leftArm_x;
-							document.getElementById("rightArm_x").innerHTML = rightArm_x;
-							document.getElementById("leftArm_y").innerHTML = leftArm_y;
-							document.getElementById("rightArm_y").innerHTML = rightArm_y;
+				var left_ind = '';
+				var right_ind = '';
+				var stop_ind = '';
+				var up_ind = '';
+				var down_ind = '';
 
-							document.getElementById("label_display").src = "images/" + direction;           
-	
-							document.getElementById("stop").innerHTML = stop_ind;
-							document.getElementById("left").innerHTML = left_ind;
-							document.getElementById("right").innerHTML = right_ind;
-							document.getElementById("up").innerHTML = up_ind;
-							document.getElementById("down").innerHTML = down_ind;	
 
-						}
+				if (leftArm_x > 60) {
+					left_ind = 'detected';
+					direction = 'left.png'
+				}
 
-	
-						if (modelType=='postures') {
+				if (rightArm_x > 60) {
+					right_ind = 'detected';
+					direction = 'right.png';
+				}
 
-							sample_predict = new Array(1)
-							sample_predict[0] = new Array(12)
+				if ((leftArm_x > 60) & (rightArm_x > 60)) {
+					stop_ind = 'detected';
+					direction = 'stop.png';
+				}
 
-							sample_predict[0][0] = leftShoulder_x;
-							sample_predict[0][1] = leftShoulder_y;
-							sample_predict[0][2] = rightShoulder_x;
-							sample_predict[0][3] = rightShoulder_y;
-							sample_predict[0][4] = leftElbow_x;
-							sample_predict[0][5] = leftElbow_y;
-							sample_predict[0][6] = rightElbow_x;
-							sample_predict[0][7] = rightElbow_y;
-							sample_predict[0][8] = leftWrist_x;
-							sample_predict[0][9] = leftWrist_y;
-							sample_predict[0][10] = rightWrist_x;
-							sample_predict[0][11] = rightWrist_y;
 
-							predictFromModel(sample_predict);
-							
-						}
+				if ((leftArm_y > 100) & (rightArm_y > 100))  {
+					up_ind = 'detected';
+					direction = 'up.png';
+				}
 
-						document.getElementById("leftShoulder_x").innerHTML = leftShoulder_x;
-						document.getElementById("leftShoulder_y").innerHTML = leftShoulder_y;
-						document.getElementById("rightShoulder_x").innerHTML = rightShoulder_x;
-						document.getElementById("rightShoulder_y").innerHTML = rightShoulder_y;
-						document.getElementById("leftElbow_x").innerHTML = leftElbow_x;
-						document.getElementById("leftElbow_y").innerHTML = leftElbow_y;
-						document.getElementById("rightElbow_x").innerHTML = rightElbow_x;
-						document.getElementById("rightElbow_y").innerHTML = rightElbow_y;
-						document.getElementById("leftWrist_x").innerHTML = leftWrist_x;
-						document.getElementById("leftWrist_y").innerHTML = leftWrist_y;
-						document.getElementById("rightWrist_x").innerHTML = rightWrist_x;
-						document.getElementById("rightWrist_y").innerHTML = rightWrist_y;
+				if ((leftArm_y < -100) & (rightArm_y < -100))  {
+					down_ind = 'detected';
+					direction = 'down.png';
+				}
+
+				document.getElementById("leftArm_x").innerHTML = leftArm_x;
+				document.getElementById("rightArm_x").innerHTML = rightArm_x;
+				document.getElementById("leftArm_y").innerHTML = leftArm_y;
+				document.getElementById("rightArm_y").innerHTML = rightArm_y;
+
+				document.getElementById("label_display").src = "images/" + direction;           
+
+				document.getElementById("stop").innerHTML = stop_ind;
+				document.getElementById("left").innerHTML = left_ind;
+				document.getElementById("right").innerHTML = right_ind;
+				document.getElementById("up").innerHTML = up_ind;
+				document.getElementById("down").innerHTML = down_ind;	
+
+			}
+
+
+			if (modelType=='postures') {
+
+				sample_predict = new Array(1)
+				sample_predict[0] = new Array(12)
+
+				sample_predict[0][0] = leftShoulder_x;
+				sample_predict[0][1] = leftShoulder_y;
+				sample_predict[0][2] = rightShoulder_x;
+				sample_predict[0][3] = rightShoulder_y;
+				sample_predict[0][4] = leftElbow_x;
+				sample_predict[0][5] = leftElbow_y;
+				sample_predict[0][6] = rightElbow_x;
+				sample_predict[0][7] = rightElbow_y;
+				sample_predict[0][8] = leftWrist_x;
+				sample_predict[0][9] = leftWrist_y;
+				sample_predict[0][10] = rightWrist_x;
+				sample_predict[0][11] = rightWrist_y;
+
+				predictFromModel(sample_predict);
+				
+			}
+
+			document.getElementById("leftShoulder_x").innerHTML = leftShoulder_x;
+			document.getElementById("leftShoulder_y").innerHTML = leftShoulder_y;
+			document.getElementById("rightShoulder_x").innerHTML = rightShoulder_x;
+			document.getElementById("rightShoulder_y").innerHTML = rightShoulder_y;
+			document.getElementById("leftElbow_x").innerHTML = leftElbow_x;
+			document.getElementById("leftElbow_y").innerHTML = leftElbow_y;
+			document.getElementById("rightElbow_x").innerHTML = rightElbow_x;
+			document.getElementById("rightElbow_y").innerHTML = rightElbow_y;
+			document.getElementById("leftWrist_x").innerHTML = leftWrist_x;
+			document.getElementById("leftWrist_y").innerHTML = leftWrist_y;
+			document.getElementById("rightWrist_x").innerHTML = rightWrist_x;
+			document.getElementById("rightWrist_y").innerHTML = rightWrist_y;
       	}
 
       })
@@ -310,28 +282,52 @@
 	}
 	
 
-	function loadLabels(video)	{
-	// loads the labels of a video into the 'label' array (with 100ms intervals)
+	function getLabel(videoTime) {
+		for (var i in label) {
+			if ((videoTime > label[i][0] & videoTime < label[i][1])) {
+				//document.getElementById("video_position").innerHTML = String(videoTime) + ': ' + String(label[i][2]) + ' | ' + String(label[i][0]) + ' | ' + String(label[i][1]);
+				return label[i][2];
+			}
+		}
 
-			d3.csv("videos/" + video + ".csv").then(function(data) {
+	}
+
+	function loadLabels(videoId)	{
+	// loads the labels of a video into the 'label' array
+
+		d3.csv("videos/labels_" + videoId + ".csv").then(function(data) {
 
 			var i;
 			for (i = 0; i < data.length; i++) { 
-			    label[i] = parseInt(data[i]["label"]);
+				label[i] = [];
+				label[i][0] = parseFloat(data[i]["from"]);
+				label[i][1] = parseFloat(data[i]["to"]);
+				label[i][2] = parseInt(data[i]["label"]);
 			}
-		  //console.log(data);
 
 		});
+
+
+		// old variant
+		//d3.csv("videos/" + video + ".csv").then(function(data) {
+
+		//	var i;
+		//	for (i = 0; i < data.length; i++) { 
+		//	    label[i] = parseInt(data[i]["label"]);
+		//	}
+
+		//});
 		
 	}
 
 
-	function showLabel(interval) {
+	function showLabel(videoTime) {
 	// displays the image (left, right etc.) corresponding to a label
 			
 		var img_src = 'stop.png';
+		var labelId = getLabel(videoTime)
 		
-		switch(label[interval]) {
+		switch(labelId) {
 		    case 0: // stop
 		        img_src = 'stop.png';
 		        break;
@@ -351,7 +347,7 @@
 		        img_src = 'stop.png';
 		}		
 		
-		document.getElementById("label_display").src = "images/" + img_src;		
+		document.getElementById("label_display").src = "images/" + img_src;	
 		
 	}
 	
@@ -391,4 +387,28 @@
 		document.getElementById("up").innerHTML = arr[3];
 		document.getElementById("down").innerHTML = arr[4];
 
+	}
+
+
+	function enableDisableVideoSelection() {
+		select = document.getElementById("selected_video");
+		if (select.disabled) {
+			select.disabled = 0
+		}
+		else {
+			select.disabled = 1
+		}
+	}
+
+
+	function showHideSeqLength() {
+		//alert(document.getElementById("posture").checked);
+		if (document.getElementById("posture").checked) {
+			document.getElementById("seq_length").style.display = "none";
+			document.getElementById("seq_length_label").innerHTML = "";
+		}
+		else {
+			document.getElementById("seq_length").style.display = "inline";
+			document.getElementById("seq_length_label").innerHTML = "Sequence Length:";
+		}
 	}
