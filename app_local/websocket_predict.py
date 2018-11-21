8c53920f2171c74fe6b714880593caf630cbaae0
Christian Wegmann <christian.wegmann@hispeed.ch>
2018-11-21 18:46:21 +0100

    integration of gesture model

--- app_local/websocket_predict.py
+++ app_local/websocket_predict.py
@@ -1,5 +1,5 @@
 # import required libraries
-from module import XCentralizer, YCentralizer, YScaler
+from module import XCentralizer, YCentralizer, YScaler, GestureTransformer
 from keras.models import load_model
 import asyncio
 import websockets
@@ -17,7 +17,7 @@ warnings.filterwarnings("ignore")
 
 # set debugging parameters
 virtual_flight = True  # flight commands are printed, but not sent to drone
-model_type = 'posture'  # allowed values are 'delta', 'posture', 'gesture'
+model_type = 'gesture'  # allowed values are 'delta', 'posture', 'gesture'
 
 # connect to drone and set status flags
 drone_last_action = time.time()
@@ -34,14 +34,14 @@ movements = {0: 'not detected',
              6: 'land'}
 
 # load model and initiate pipeline
-if ((model_type == 'posture') | (model_type == 'gesture')):
+if (model_type == 'posture'):
 
     model = load_model('../models/model_' + model_type + '.h5')
 
     cols_x = ['leftShoulder_x',
               'rightShoulder_x',
               'leftWrist_x',
-              'rightWrist_x',  # elbow to be added
+              'rightWrist_x',
               'leftHip_x',
               'rightHip_x']
 
@@ -51,6 +51,26 @@ if ((model_type == 'posture') | (model_type == 'gesture')):
                                         YCentralizer(cols_y),
                                         YScaler())
 
+if (model_type == 'gesture'):
+
+    model = load_model('../models/model_' + model_type + '.h5')
+
+    cols_x = ['leftShoulder_x',
+              'rightShoulder_x',
+              'leftWrist_x',
+              'rightWrist_x',
+              'leftHip_x',
+              'rightHip_x',
+              'leftElbox_x',
+              'rightElbow_x']
+
+    cols_y = [col.replace('x', 'y') for col in cols_x]
+
+    cols = cols_x + cols_y
+
+    pose_df = pd.DataFrame(columns=cols)
+    processing_pipeline = make_pipeline(GestureTransformer(cols))
+
 
 async def consumer_handler(websocket, path):
 
@@ -58,6 +78,7 @@ async def consumer_handler(websocket, path):
     print(model_type)
 
     print('Accepting incoming snapshots. Waiting for take-off signal.')
+#    try:
     async for pose_json in websocket:
         pose_dict = json_to_dict(pose_json)
         if model_type == 'delta':
@@ -66,6 +87,8 @@ async def consumer_handler(websocket, path):
             steer_drone(predict_movement_model_posture(pose_dict))
         if model_type == 'gesture':
             steer_drone(predict_movement_model_gesture(pose_dict))
+    # except:
+    #     print('Websocket connection terminated. Please re-connect.')
 
 
 def steer_drone(movement):
@@ -83,7 +106,7 @@ def steer_drone(movement):
             threading.Thread(target=drone_move_forward).start()
         if movement == 3:
             threading.Thread(target=drone_flip).start()
-        if movement == 4:
+        if movement == 4:  # to be renamed to 'left' and 'right'
             threading.Thread(target=drone_rotate_cw).start()
         if movement == 5:
             threading.Thread(target=drone_rotate_ccw).start()
@@ -133,7 +156,7 @@ def drone_land():
         drone.land()
 
 
-def json_to_dict(pose_json):
+def json_to_dict(pose_json): # to be added: handling for case when no coordinates come through
 
     x = json.loads(pose_json)
     pose_dict = {}
@@ -196,7 +219,18 @@ def predict_movement_model_posture(pose_dict):
 
 def predict_movement_model_gesture(pose_dict):
 
+    global pose_df
     movement = 0
+
+    pose_df = pose_df.append(pd.DataFrame(pose_dict, index=[0]))
+
+    if len(pose_df) > 17:
+        pose_df = pose_df.iloc[1:]
+
+    if len(pose_df) == 17:
+        pose_np = pose_df.values.reshape(1, 17, 16)
+        processing_pipeline.fit_transform(pose_np)
+        movement = np.argmax(model.predict(pose_np)[0])
 
     return movement
 
